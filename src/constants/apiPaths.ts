a35ce1ad0bff4853ec6a85954b135f1523e121aa
Taras Khmelovskyi <khmelovski@gmail.com>
2020-11-15 17:37:15 +0200

    feat: Update import api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://etmik45sra.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://8njuk7wwgf.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://etmik45sra.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
